389714be3ff07ac873eebbc93dc5a6614c6a0141
Shubham Utwal <shubhamutwal@gmail.com>
2016-11-20 18:57:52 +0530

    Refactor: Using generic class-based views

--- meatmarketrestservice/views.py
+++ meatmarketrestservice/views.py
@@ -1,41 +1,20 @@
 from rest_framework import generics
-from rest_framework import mixins
 
 from meatmarketrestservice.models import Item
 from meatmarketrestservice.serializers import ItemSerializer
 
 
-class ItemList(mixins.ListModelMixin,
-               mixins.CreateModelMixin,
-               generics.GenericAPIView):
+class ItemList(generics.ListCreateAPIView):
     """
     List all items or create a new item
     """
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class ItemDetail(mixins.RetrieveModelMixin,
-                 mixins.UpdateModelMixin,
-                 mixins.DestroyModelMixin,
-                 generics.GenericAPIView):
+class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete an item
     """
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
