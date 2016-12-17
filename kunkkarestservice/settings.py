5d7452b758ee835955e81160d4deb3daeee90100
Shubham Utwal <shubhamutwal@gmail.com>
2016-12-17 19:07:30 +0530

    djangorestframework-jwt support added

--- kunkkarestservice/settings.py
+++ kunkkarestservice/settings.py
@@ -123,7 +123,15 @@ STATIC_URL = '/static/'
 
 
 # django-rest-framework configurations
+# Lookout http://getblimp.github.io/django-rest-framework-jwt/ for more JWT related configurations
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
     'PAGE_SIZE': 10
 }
--- kunkkarestservice/urls.py
+++ kunkkarestservice/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('meatmarketrestservice.urls')),
+    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-refresh/', refresh_jwt_token),
 ]
